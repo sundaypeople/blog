7097ce53c11d2890a834c19d0a843140ee193e22
Laininthewired <flx503@outlook.jp>
2025-03-07 16:10:51 +0900

    Typographyの設定を調整

--- source/tailwind.config.js
+++ source/tailwind.config.js
@@ -14,7 +14,22 @@ module.exports = {
         DEFAULT: {
           css: {
             fontSize: '1rem', // フォントサイズを変更
-            lineHeight: '1', // 行間を変更
+            lineHeight: '1rem', // 行間を変更
+            hr: {
+              marginTop: "1rem",
+              marginBottom: "2rem"
+            },
+            h1 :{
+              marginTop: "5rem",
+              marginBottom: "1rem"
+            },
+            h2 : {
+              marginTop: "3rem",
+              marginBottom: "1rem"
+            },
+            p: {
+              margin: "1rem 0"
+            }
             // 他にも調整したいスタイルをここで指定
           },
         },
